86fac36cac0ba579da3ea67472360c41053e5ae3
osamakhalil <osaamakhalil1234@gmail.com>
2022-09-20 09:59:56 +0200

    :white_check_mark: Set up view model with di

--- app/src/main/java/com/example/fakestoreapp/di/module/RemoteModule.kt
+++ app/src/main/java/com/example/fakestoreapp/di/module/RemoteModule.kt
@@ -2,11 +2,15 @@ package com.example.fakestoreapp.di.module
 
 import com.example.fakestoreapp.remote.api.ProductApiService
 import dagger.Module
+import dagger.Provides
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 abstract class RemoteModule {
     companion object {
+        @Provides
+        @Singleton
         fun provideProductApiService(retrofit: Retrofit): ProductApiService {
             return retrofit.create(ProductApiService::class.java)
         }
--- app/src/main/java/com/example/fakestoreapp/di/module/ViewModelsModule.kt
+++ app/src/main/java/com/example/fakestoreapp/di/module/ViewModelsModule.kt
@@ -1,15 +1,25 @@
 package com.example.fakestoreapp.di.module
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.fakestoreapp.di.ViewModelKey
 import com.example.fakestoreapp.di.ViewModelProviderFactory
+import com.example.fakestoreapp.presentation.products.viewmodel.ProductsViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
-class ViewModelsModule {
+@Module
+abstract class ViewModelsModule {
     @Module
     abstract class ViewModelsModule {
 
         @Binds
         abstract fun bindViewModelFactory(viewModelFactory: ViewModelProviderFactory?): ViewModelProvider.Factory?
     }
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProductsViewModel::class)
+    abstract fun bindProductsViewModel(viewModel: ProductsViewModel): ViewModel?
 }

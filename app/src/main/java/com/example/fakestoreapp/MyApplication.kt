3d1d1c103275d7032ad2d54d93e80ea76eb91b2e
osamakhalil <osaamakhalil1234@gmail.com>
2022-09-20 09:55:29 +0200

    :white_check_mark: Added timber

--- app/src/main/java/com/example/fakestoreapp/MyApplication.kt
+++ app/src/main/java/com/example/fakestoreapp/MyApplication.kt
@@ -3,6 +3,7 @@ package com.example.fakestoreapp
 import android.app.Application
 import com.example.fakestoreapp.di.component.AppComponent
 import com.example.fakestoreapp.di.component.DaggerAppComponent
+import timber.log.Timber
 
 class MyApplication : Application() {
 
@@ -13,6 +14,7 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         provideAppComponent().inject(this)
+        Timber.plant(Timber.DebugTree())
     }
 
     fun provideAppComponent() = appComponent

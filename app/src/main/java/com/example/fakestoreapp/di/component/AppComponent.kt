55ef02634b817df0d9b5e96d07b6445b52bd725f
osamakhalil <osaamakhalil1234@gmail.com>
2022-09-20 09:55:01 +0200

    :white_check_mark: Added product fragment on di

--- app/src/main/java/com/example/fakestoreapp/di/component/AppComponent.kt
+++ app/src/main/java/com/example/fakestoreapp/di/component/AppComponent.kt
@@ -6,6 +6,8 @@ import com.example.fakestoreapp.MyApplication
 import com.example.fakestoreapp.di.module.AppModule
 import com.example.fakestoreapp.di.module.DataModule
 import com.example.fakestoreapp.di.module.RemoteModule
+import com.example.fakestoreapp.di.module.ViewModelsModule
+import com.example.fakestoreapp.presentation.products.view.ProductsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -15,7 +17,8 @@ import javax.inject.Singleton
     modules = [
         AppModule::class,
         RemoteModule::class,
-        DataModule::class
+        DataModule::class,
+        ViewModelsModule::class
     ]
 )
 interface AppComponent {
@@ -23,6 +26,8 @@ interface AppComponent {
     fun inject(app: MyApplication)
 
     fun inject(activity: MainActivity)
+
+    fun inject(fragment: ProductsFragment)
 
     @Component.Factory
     interface Factory {
